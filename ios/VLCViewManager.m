30701b590afbd950b23e06979024011676f4da8c
dinh tho <dinhtho@dinhs-iMac.local>
2020-10-02 08:25:34 +0700

    update

---
+++ ios/VLCViewManager.m
@@ -0,0 +1,8 @@
+//
+//  VLCViewManager.m
+//  nativeIOS
+//
+//  Created by dinh tho on 10/1/20.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/VLCViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  VLCViewManager.swift
+//  nativeIOS
+//
+//  Created by dinh tho on 10/1/20.
+//
+
+import Foundation
